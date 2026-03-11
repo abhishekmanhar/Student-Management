01bae9a8021dd8ade0b9c33fa71ccb54d8b5aa80
Abhishek Manhar <abhimanharkhan25@gmail.com>
2026-03-11 18:52:03 +0530

    Fix framer motion import

--- frontend/src/components/StudentModal.jsx
+++ frontend/src/components/StudentModal.jsx
@@ -1,4 +1,4 @@
-
+import { motion } from "framer-motion";
 import { useForm } from "react-hook-form"
 import { updateStudent, createStudent } from "../services/api"
 import toast from "react-hot-toast"
